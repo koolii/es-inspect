7aa244c4162d14b60c64f6f388b6f6657c6593d5
koolii <kuriyama@createentrance.com>
2017-12-15 12:21:50 +0100

    :tada: add import/export samples

---
+++ src/import-export/index.mjs
@@ -0,0 +1,10 @@
+import { Calc, hello, User } from './calc';
+
+console.log(hello);
+
+console.log(Calc.add(100, 50));
+console.log(Calc.multi(100, 50));
+console.log(Calc.sub(100, 50));
+
+const bob = new User('bob', 25);
+bob.say();
